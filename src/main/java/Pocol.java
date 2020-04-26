987e4f1cfe7f551f4c275aee7dfd4328e76096ec
Bogdan - Adrian Pocol <bogdan.pocol@gmail.com>
2020-04-26 11:20:00 +0300

    Commit 18 from 26.04.2020 11:19 on master, added Pocol class that contains lombok, java io./nio. and tinylog

---
+++ src/main/java/Pocol.java
@@ -0,0 +1,13 @@
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.pmw.tinylog.Logger;
+
+public class Pocol {
+}
